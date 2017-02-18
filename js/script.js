166f12c2f8d3423f6f2d75c172238cdc3109a24c
Tye Campbell <tylerjoelcampbell@gmail.com>
2017-02-18 10:59:16 -0600

    Finished location page. Added directions and images

--- js/script.js
+++ js/script.js
@@ -503,3 +503,20 @@ function sendRequest() {
 	ajax.send(formdata);
 }
 
+
+/* ---------- Store Location Page ---------- */
+
+function initMap(){
+	var uluru = {lat: 41.686894, lng: -93.790856};
+
+	var map = new google.maps.Map(document.getElementById("map"), {
+		zoom: 16, 
+		center: uluru
+	});
+	var marker = new google.maps.Marker({
+		position: uluru,
+		map: map
+	})
+
+}
+
--- location.php
+++ location.php
@@ -6,6 +6,42 @@
 
   ?>
 
-<h1>Store Location</h1>
+<div class="container-fluid">
+	<div class="row">
+		<div id="map">
+		</div>
+	</div>
+</div>
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-6 store">
+			<img src="images/store-front.png" class="img-responsive img-circle center-block store-img">
+		</div>
+		<div class="col-md-6">
+			<h2 class="text-center">Store Location</h2>
+			<p class="text-center">Stop by and see us! We have fuzzy friends begging for attention!</p>
+			<div class="directions">
+				<h4>Directions from Des Moines</h4>
+				<ul>
+					<li>Take I35 North to Exit 127 toward Urbandale/Grimes</li>
+					<li>Continue on IA-141 West</li>
+					<li>Drive to IA-44 W/E 1st Stree in Grimes</li>
+					<li>Turn Right onto IA-141 W/NW Urbandale Drive</li>
+					<li>Take The IA-44 Ramp to Grimes/Saylorville Lake</li>
+					<li>Turn Left ont SE Main Street</li>
+					<li>Located on the left side, on block south</li>
+				</ul>
+				<address class="text-center address">Sandy's Pet Shop<br> 215 Main Street<br> Grimes, IA 50111</address>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="container-fluid">
+	<div class="row">
+		<img src="images/store-inside-view.png" class="img-responsive center-block inside-img">
+	</div>
+</div>
 
   <?php require 'Includes/footer.php'; ?>
