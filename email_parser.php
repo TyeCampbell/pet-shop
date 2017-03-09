7252cfb84663a2f9e7ec07b8daf404c157889102
Tye Campbell <tylerjoelcampbell@gmail.com>
2017-03-09 13:12:30 -0600

    removed password/email securtiy

--- email_parser.php
+++ email_parser.php
@@ -22,14 +22,14 @@ if( isset($_POST['fname']) && isset($_POST['lname']) && isset($_POST['email']) &
 	$mail->isSMTP();
 	$mail->Host = 'smtp.gmail.com';
 	$mail->SMTPAuth = true;
-	$mail->Username = 'sandys.petshop.webucator@gmail.com';
-	$mail->Password = 'pwdpwd1234';
+	$mail->Username = '*******@gmail.com';
+	$mail->Password = '**********';
 	$mail->SMTPSecure = 'tls';
 	$mail->Port = 587;
 
 	$mail->setFrom($email, $fname . " " . $lname);
 	$mail->addReplyTo($email, $fname . " " . $lname);
-	$mail->addAddress('sandys.petshop.webucator@gmail.com');
+	$mail->addAddress('*******@gmail.com');
 
 	$mail->isHTML(true);
 
